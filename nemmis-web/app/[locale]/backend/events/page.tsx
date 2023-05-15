8c0dc00ffb14b74a7d0511c22f5daf0db822bdf4
Edu Gelabert Olives <61160511+EduGelabert@users.noreply.github.com>
2023-05-15 22:57:15 +0200

    [WIP]

--- nemmis-web/app/[locale]/backend/events/page.tsx
+++ nemmis-web/app/[locale]/backend/events/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Table } from "@/components/Table";
-import { Event, PrismaClient } from "@prisma/client";
+import prisma from "@/utils/prisma";
+import { Event } from "@prisma/client";
 import { GetStaticProps, NextPage } from "next";
 import { useEffect } from "react";
+import Table from "../../table";
 
 interface Props {
   Events: Event[];
@@ -11,14 +12,14 @@ interface Props {
 
 const Index: NextPage<Props> = ({ Events }) => {
   useEffect(() => {
-    console.log(Events);
+    console.log(Events, "page");
   }, [Events]);
   return <Table components={Events}></Table>;
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const prisma = new PrismaClient();
   const events = await prisma.event.findFirst();
+  console.log(events, "getstaticprops");
   return { props: { events } };
 };
 
--- nemmis-web/components/Table.tsx
+++ nemmis-web/components/Table.tsx
@@ -1,9 +1,58 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 type Props = {
-  components?: any;
+  components?: any[];
 };
 
 export const Table = ({ components }: Props) => {
-  return <table></table>;
+  /* 
+    id: bigint;
+    title_text_id: bigint;
+    description_text_id: bigint;
+    date: Date; */
+
+  const [columns, setColumns] = useState<any[]>([]);
+  useEffect(() => {
+    components && setColumns(Object.keys(components[0]));
+
+    console.log(components, "table");
+  }, [components]);
+
+  return (
+    <table>
+      <tr></tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+      <tr>
+        <td>Ernst Handel</td>
+        <td>Roland Mendel</td>
+        <td>Austria</td>
+      </tr>
+      <tr>
+        <td>Island Trading</td>
+        <td>Helen Bennett</td>
+        <td>UK</td>
+      </tr>
+      <tr>
+        <td>Laughing Bacchus Winecellars</td>
+        <td>Yoshi Tannamuri</td>
+        <td>Canada</td>
+      </tr>
+      <tr>
+        <td>Magazzini Alimentari Riuniti</td>
+        <td>Giovanni Rovelli</td>
+        <td>Italy</td>
+      </tr>
+    </table>
+  );
 };
