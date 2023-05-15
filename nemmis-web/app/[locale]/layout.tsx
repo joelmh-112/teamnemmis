8d8e1dae9ce3b7679362b56d7c7818a1d5b83327
JoelMH <joel.martinez@menorcaon.com>
2023-05-15 21:47:02 +0200

    [FEAT] messages next-intl deepmerged

--- nemmis-web/app/[locale]/layout.tsx
+++ nemmis-web/app/[locale]/layout.tsx
@@ -2,15 +2,19 @@ import { NextAuthProvider } from "@/components/NextAuthProvider";
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import './../globals.css'
+import deepmerge from "deepmerge";
 
 export function generateStaticParams() {
   return [{ locale: "en" }, { locale: "de" }];
 }
 
 export default async function LocaleLayout({ children, params: { locale } }: { children: React.ReactNode; params: { locale: string } }) {
-  let messages;
+  let messages,defaultMessages,userMessages;
   try {
-    messages = (await import(`../../messages/${locale}.json`)).default;
+    defaultMessages = (await import(`../../messages/es.json`)).default;
+    userMessages = (await import(`../../messages/${locale}.json`)).default;
+    messages = deepmerge(defaultMessages, userMessages);
+
   } catch (error) {
     notFound();
   }
